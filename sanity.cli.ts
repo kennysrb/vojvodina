5cb4b19cd0f02bec25cfe777728e439293044176
Nikola <kennysrb@gmail.com>
2026-04-20 17:14:06 +0200

    feat: migrate to new vojvodina-hc Sanity project (c168eju0)

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: "lvi63s3m",
+    projectId: "c168eju0",
     dataset: "production",
   },
 });
